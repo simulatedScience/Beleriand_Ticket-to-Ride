384b0899b32ce96060e268bb78feffc87cc3687d
simulatedScience <62523381+simulatedScience@users.noreply.github.com>
2022-12-29 13:53:03 +0100

    graph particle repulsion working

--- coding/ttr_map_maker/graph_particle.py
+++ coding/ttr_map_maker/graph_particle.py
@@ -4,51 +4,95 @@ base class for particles in a particle graph
 from typing import Tuple
 
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon as PolygonPatch
+from shapely.geometry import Polygon
 
 class graph_particle:
   def __init__(self,
-        position: Tuple[float, float],
+        position: np.ndarray = np.array([0, 0]),
         rotation: float = 0,
         mass: float = 1,
-        bounding_box_size: Tuple[float, float] = (1, 1)):
-    self.position = position # position of particle's center
-    self.rotation = rotation # rotation of particle in radians
-    self.velocity = (0, 0)
-    self.angular_velocity = 0
-    self.acceleration = (0, 0)
-    self.angular_acceleration = 0
+        bounding_box_size: Tuple[float, float] = (1, 1),
+        interaction_radius: float = 1):
+    """
+    initialize a particle
+    moment of inertia is calculated from bounding box size and mass assuming a uniform density and a rectangular shape
+
+    args:
+      position (np.ndarray): position of particle's center
+      rotation (float): rotation of particle in radians, counter-clockwise from positive x-axis
+      mass (float): mass of particle
+      bounding_box_size (Tuple[float, float]): size of particle's bounding box (width, height)
+      interaction_radius (float): particle's interaction radius. Particles will only interact with other particles where the distance between their bounding boxes is less than `interaction_radius`.
+    """
+    # translation properties
+    self.position = position.astype(np.float64) # position of particle's center
+    self.velocity = np.zeros(2)
+    self.acceleration = np.zeros(2)
     self.mass = mass
+    # rotation properties
+    self.rotation = rotation # rotation of particle in radians
+    self.angular_velocity = 0
+    self.angular_acceleration = 0
+    self.inertia = mass * (bounding_box_size[0] ** 2 + bounding_box_size[1] ** 2) / 12 # moment of inertia
+    
+    self.velocity_decay = 0.99 # velocity decay factor
+    # bounding box properties
     self.bounding_box_size = bounding_box_size # corners of bounding box, in counter-clockwise order
-    self.bounding_box = self.update_bounding_box()
+    self.bounding_box, self.bounding_box_polygon = self.update_bounding_box()
+
+    # variables for Verlet list algorithm
+    self.neighbors = []
+    self.interaction_radius = interaction_radius
 
 
   def __str__(self):
-    return f"Particle at {self.position}"
+    return f"Particle at\t {self.position} with mass\t {self.mass} and inertia\t {self.inertia}."
 
 
-  def interact(self, other: "graph_particle"):
+  def interact(self, other: "graph_particle") -> Tuple[np.ndarray, float]:
     """
-    interact with another particle
-    forces are calculated from the amount of overlap between the particles' bounding boxes
+    Interact with another particle.
+    Only interact with particles where the distance between their bounding boxes is less than `self.interaction_radius`.
+    Forces are calculated from the amount of overlap between the particles' bounding boxes.
+    This interaction is not symmetric, so `self` particle will be affected by `other` particle differently than `other` particle will be affected by `self` particle.
 
     args:
       other (graph_particle): other particle to interact with
 
     returns:
-      (Tuple[float, float]): force to apply to self particle
+      (np.ndarray) acceleration to apply to `self` particle
+      (float) angular acceleration to apply to `self` particle
     """
-    # get bounding boxes
-    self_bounding_box = self.get_bounding_box()
-    other_bounding_box = other.get_bounding_box()
+    # check if particles are close enough to interact
+    self_polygon = self.get_bounding_box_polygon()
+    other_polygon = other.get_bounding_box_polygon()
+    if self_polygon.distance(other_polygon) > self.interaction_radius:
+      # if distance between bounding boxes is greater than interaction radius
+      return np.zeros(2), 0
 
     # get overlap
-    overlap = self.get_overlap(self_bounding_box, other_bounding_box)
+    overlap_center, overlap_area = get_box_overlap(self_polygon, other_polygon)
+    if overlap_area <= 0:
+      # if no overlap
+      return np.zeros(2), 0
 
-    # calculate force
-    force = overlap * 1000
+    # calculate forces
+    overlap_vector = overlap_center - self.position
+    translation_force = -overlap_vector * overlap_area
+    translation_acceleration = translation_force / self.mass
+    # rotation force perpendicular to translation force
+    center_center = other.position - self.position
+    rotation_axis = np.array([0, 0, np.cross(center_center, overlap_vector)])
+    rotation_axis /= np.linalg.norm(rotation_axis)
+    tangential_force = np.cross(rotation_axis, overlap_vector) * overlap_area
+    torque = np.linalg.norm(overlap_vector) * np.linalg.norm(tangential_force) * (-rotation_axis[2])
+    angular_acceleration = torque / self.inertia
 
-    return force
-
+    self.acceleration = translation_acceleration
+    self.angular_acceleration = angular_acceleration
+    return translation_acceleration, angular_acceleration
 
 
   def update(self, dt: float):
@@ -56,23 +100,52 @@ class graph_particle:
     update particle's position and velocity
     """
     if np.linalg.norm(self.velocity) > 0 or np.linalg.norm(self.acceleration) > 0 or self.angular_velocity > 0 or self.angular_acceleration > 0:
-      self.moved = True
+      # update velocity
+      self.velocity += self.acceleration * dt
+      self.angular_velocity += self.angular_acceleration * dt
 
-    # update velocity
-    self.velocity += self.acceleration * dt
-    self.angular_velocity += self.angular_acceleration * dt
+      # update position
+      self.position += self.velocity * dt
+      self.rotation += self.angular_velocity * dt
+
+      # update bounding box
+      self.bounding_box, self.bounding_box_polygon = self.update_bounding_box()
+
+      # reduce velocities using friction
+      self.velocity *= self.velocity_decay * dt
+      self.angular_velocity *= self.velocity_decay * dt
 
 
-
-  def update_bounding_box(self) -> np.ndarray:
+  def get_bounding_box(self) -> np.ndarray:
     """
-    update bounding box of particle stored in `self.bounding_box`
+    get bounding box of particle stored in `self.bounding_box`
 
     returns:
       (np.ndarray): bounding box as numpy array containing it's corners, in counter-clockwise order
     """
+    return self.bounding_box
+
+
+  def get_bounding_box_polygon(self) -> Polygon:
+    """
+    get bounding box of particle stored in `self.bounding_box_polygon`
+
+    returns:
+      (shapely.geometry.Polygon): bounding box as shapely polygon
+    """
+    return self.bounding_box_polygon
+
+
+  def update_bounding_box(self) -> Tuple[np.ndarray, Polygon]:
+    """
+    recalculate bounding box of particle from `self.position`, `self.rotation`, and `self.bounding_box_size`
+
+    returns:
+      (np.ndarray): bounding box as numpy array containing it's corners, in counter-clockwise order
+      (shapely.geometry.Polygon): bounding box as shapely polygon
+    """
     width, height = self.bounding_box_size
-    self.bounding_box = np.array([
+    bounding_box = np.array([
       [width / 2, height / 2],
       [width / 2, -height / 2],
       [-width / 2, -height / 2],
@@ -84,9 +157,104 @@ class graph_particle:
       [np.cos(self.rotation), -np.sin(self.rotation)],
       [np.sin(self.rotation), np.cos(self.rotation)]
     ])
-    self.bounding_box = self.bounding_box @ rotation_matrix
+    bounding_box = bounding_box @ rotation_matrix
 
     # translate bounding box
-    self.bounding_box += self.position
+    bounding_box += self.position
 
-    return self.bounding_box
+    return bounding_box, Polygon(bounding_box)
+
+  
+  def draw_bounding_box(self, ax: plt.Axes, color: str = "", alpha: float = 0.3, zorder: int = 1):
+    """
+    draw bounding box of particle on `ax`
+
+    args:
+      ax (matplotlib.axes.Axes): axis to draw on
+    """
+    polygon_patch = PolygonPatch(
+        self.get_bounding_box(),
+        facecolor=color,
+        edgecolor=color,
+        alpha=alpha,
+        zorder=zorder
+    )
+    ax.add_patch(polygon_patch)
+
+
+def get_box_overlap(box1_poly: Polygon, box2_poly: Polygon) -> Tuple[np.ndarray, float]:
+    """
+    get overlap between two bounding boxes
+
+    args:
+      box1_poly (shapely.geometry.Polygon): first bounding box as shapely polygon
+      box2_poly (shapely.geometry.Polygon): second bounding box as shapely polygon
+
+    returns:
+      (Tuple[np.ndarray, float]): center of overlap and area of overlap
+    """
+    # Calculate overlapping polygon
+    overlap_poly = box1_poly.intersection(box2_poly)
+
+    # Check if there is no overlap
+    if overlap_poly.is_empty:
+        return np.zeros((2,)), 0.0
+
+    # Calculate area of overlap
+    overlap_area = overlap_poly.area
+
+    # Calculate center of overlap
+    overlap_center = np.array([overlap_poly.centroid.x, overlap_poly.centroid.y])
+
+    return overlap_center, overlap_area
+
+
+if __name__ == "__main__":
+  # create particles
+  particle1 = graph_particle(position=np.array([5, 3]), rotation=0, mass=1, bounding_box_size=np.array([8, 2]))
+  particle2 = graph_particle(position=np.array([0.5, 0]), rotation=np.pi/2, mass=1, bounding_box_size=np.array([8, 2]))
+
+  # plot before interaction
+  fig, ax = plt.subplots()
+  ax.set_aspect('equal')
+  particle1.draw_bounding_box(ax, color="#ff0000", alpha=0.1)
+  particle2.draw_bounding_box(ax, color="#0000ff", alpha=0.1)
+
+  print("before interaction")
+  print(f"particle1.position: {particle1.position}")
+  print(f"particle1.rotation: {particle1.rotation}")
+  print(f"particle2.position: {particle2.position}")
+  print(f"particle2.rotation: {particle2.rotation}")
+
+  print(f"bounding box 1: {particle1.get_bounding_box()}")
+  # interact
+  n_steps = 50
+  dt = 2e-1
+  position_1 = np.zeros((n_steps, 2))
+  position_2 = np.zeros((n_steps, 2))
+  for n in range(n_steps):
+    translation_acceleration_1, rot_acceleration_1 = particle1.interact(particle2)
+    translation_acceleration_2, rot_acceleration_2 = particle2.interact(particle1)
+    particle1.update(dt)
+    particle2.update(dt)
+    position_1[n] = particle1.position
+    position_2[n] = particle2.position
+
+  ax.plot(position_1[:, 0], position_1[:, 1], color="#ff0000", label="particle 1 center")
+  ax.plot(position_2[:, 0], position_2[:, 1], color="#0000ff", label="particle 2 center")
+
+  print("after interaction")
+  print(f"particle1.position: {particle1.position}")
+  print(f"particle1.rotation: {particle1.rotation}")
+  print(f"particle2.position: {particle2.position}")
+  print(f"particle2.rotation: {particle2.rotation}")
+
+  print(f"bounding box 1: {particle1.get_bounding_box()}")
+  # plot after interaction
+  particle1.draw_bounding_box(ax, color="#aa0000")
+  particle2.draw_bounding_box(ax, color="#0000aa")
+
+  ax.set_xlim(-10, 10)
+  ax.set_ylim(-10, 10)
+  ax.legend()
+  plt.show()
