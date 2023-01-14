fixed rotation sign being wrong in some places, removed debug visualisation for edge rotation

coding/ttr_map_maker/drag_handler.py
@@ -67,12 +67,11 @@ class Drag_Handler:
       event.xdata = event.mouseevent.x
       event.ydata = event.mouseevent.y
       if self.current_particle is None:
-        print("weird")
+        print("Warning: encountered unexpected state in drag handler: internal state not reset properly.")
       self.on_release(event)
     # Get the rectangle artist that was picked
     self.current_artist = event.artist
     self.new_rotation_deg = 0
-    # print(f"found artist: {self.current_artist}")
     # if artist is a circle, get its center
     if isinstance(self.current_artist, (Circle, Rectangle)):
       artist_center: np.ndarray = self.current_artist.get_center()
@@ -85,7 +84,6 @@ class Drag_Handler:
     else:
       print(f"Warning: unknown artist type: {type(self.current_artist)}")
       return
-    # print(f"found artist at: {artist_center}")
     # Bind the motion and button release events to the canvas
     self.cid_1 = self.canvas.mpl_connect("motion_notify_event", self.on_motion)
     self.cid_2 = self.canvas.mpl_connect("button_release_event", self.on_release)
@@ -103,8 +101,6 @@ class Drag_Handler:
       self.current_artist = None
 
     self.canvas.mpl_disconnect(self.pick_id)
-    print(f"picked artist: {event.artist}")
-    print(f"picked a particle: {type(self.current_particle)}")
 
   def on_motion(self, event):
     """
@@ -188,11 +184,10 @@ class Drag_Handler:
     self.current_particle.set_position(np.array([x_pos, y_pos]))
     new_rotation_rad = np.deg2rad(self.new_rotation_deg)
     old_rotation_rad = self.current_particle.get_rotation()
-    self.current_particle.set_rotation(old_rotation_rad - new_rotation_rad)
+    self.current_particle.set_rotation(old_rotation_rad + new_rotation_rad)
     self.current_particle.erase()
     self.current_particle.draw(self.ax)
 
-    print(f"artist released: {self.current_artist}")
     self.current_artist = None
     self.current_particle = None
     self.pick_id = self.canvas.mpl_connect('pick_event', self.on_pick)

coding/ttr_map_maker/particle_edge.py
@@ -4,8 +4,6 @@ There is an attraction force between the node and target position as well as bet
 
 A node can be connected to other nodes by edges.
 """
-import colorsys
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -241,51 +239,12 @@ class Particle_Edge(Graph_Particle):
       plotted_image = ax.imshow(mpl_image, extent=edge_extent, zorder=zorder, picker=True)
       # rotate image using transformation
       # keep image upright
-      image_rotation, normal_vec = self.get_image_rotation()
+      image_rotation = self.get_image_rotation()
       # print(f"image rotation {self.location_1_name}-{self.location_2_name}-{self.path_index}: {image_rotation}")
       plotted_image.set_transform(
         transforms.Affine2D().rotate_around(self.position[0], self.position[1], image_rotation) + ax.transData
       )
       self.plotted_objects.append(plotted_image)
-      # debug: draw arrow to show direction of particle
-      # hue indicates direction of particle
-      color = colorsys.hsv_to_rgb((image_rotation % (2 * np.pi)) / (2 * np.pi), 1, 1)
-      self.plotted_objects.append(
-          ax.arrow(
-              self.position[0],
-              self.position[1],
-              np.cos(image_rotation),
-              np.sin(image_rotation),
-              color = color,
-              linewidth=2,
-              alpha=0.8,
-              zorder=zorder))
-      # debug: draw arrow to show direction of normal
-      # hue indicates direction of particle
-      rotation = np.arctan2(normal_vec[1], normal_vec[0])
-      color = colorsys.hsv_to_rgb((rotation % (2 * np.pi)) / (2 * np.pi), 0.8, .5)
-      self.plotted_objects.append(
-          ax.arrow(
-              self.position[0],
-              self.position[1],
-              np.cos(rotation),
-              np.sin(rotation),
-              color = color,
-              linewidth=2,
-              alpha=0.8,
-              zorder=zorder))
-    rotation = self.rotation
-    color = colorsys.hsv_to_rgb((rotation % (2 * np.pi)) / (2 * np.pi), 0.8, .5)
-    self.plotted_objects.append(
-        ax.arrow(
-            self.position[0],
-            self.position[1],
-            np.cos(rotation),
-            np.sin(rotation),
-            color = color,
-            linewidth=2,
-            alpha=0.8,
-            zorder=zorder))
 
 
   def get_image_rotation(self) -> float:
@@ -314,8 +273,6 @@ class Particle_Edge(Graph_Particle):
         visited_particle_ids.add(connected_nodes[i].get_id())
         if isinstance(connected_nodes[i], Particle_Node):
           break
-      # print(f"visited particles to find node: {visited_particle_ids}")
-      # print(f"found node {connected_nodes[i].get_id()} at {connected_nodes[i].position}")
     # calculate normal vector of direct connection between nodes
     node_1_position = connected_nodes[0].position
     node_2_position = connected_nodes[1].position
@@ -329,10 +286,11 @@ class Particle_Edge(Graph_Particle):
     # ensure that normal vector direction is always pointing upwards
     if normal_vector[1] < 0:
       normal_vector = -normal_vector
-    # if angle between normal vector and particle rotation is larger than 90°, rotate edge by 180°
+    # if normal vector is to the right of the current rotation vector, rotate by 180°
+    # this aligns the image with the normal vector
     if np.cross(normal_vector, np.array([np.cos(self.rotation), np.sin(self.rotation)])) > 0:
-      return self.rotation + np.pi, normal_vector
-    return self.rotation, normal_vector
+      return self.rotation + np.pi
+    return self.rotation
 
 
 