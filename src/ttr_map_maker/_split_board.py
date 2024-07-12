f16d9b6591e1f6ddb8fdb172c0fdfd54851146a6
simulatedScience <62523381+simulatedScience@users.noreply.github.com>
2024-07-12 10:44:39 +0200

    auto-generate split board tex file

--- src/ttr_map_maker/_split_board.py
+++ src/ttr_map_maker/_split_board.py
@@ -4,6 +4,9 @@ from tkinter import Tk, filedialog
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
+from pylatex import Document, NoEscape, MiniPage, Command, Package
+
+from _task_card_pdf_generation import add_horizontal_crop_marks, add_vertical_crop_marks
 
 def split_image_into_4_parts(image_path, output_folder):
     """
@@ -45,92 +48,287 @@ def split_image_into_4_parts(image_path, output_folder):
 
     print("The image has been split and saved to the output folder.")
 
-def split_image_nxm(
-        image_path,
-        n,
-        m,
-        total_width: float = 832, # in mm
-        total_height: float = 589, # in mm
+def add_fullpage_image(
+        doc: Document,
+        x_pos: float,
+        y_pos: float,
+        image_path: str,
+        tile_width: float = 88.9,
+        tile_height: float = 63.5,):
+    """
+    Adds a single card (front and back) to the document.
+
+    Args:
+        doc (Document): The LaTeX document.
+        front_path (str): File path to the front image.
+        back_path (str): File path to the back image.
+        flip_backside (bool): Whether to flip the back image.
+        card_width (float): Width of the card in mm.
+        card_height (float): Height of the card in mm.
+        gap (float): Gap between front and back in mm.
+        vspace (float): Vertical space between cards in mm.
+    """
+    # Back image first
+    image_command = Command(
+        'includegraphics',
+        options=NoEscape(
+            # f'height={max(tile_height, tile_width)}mm, ' + 
+            # f'width={min(tile_height, tile_width)}mm, ' +
+            f'height={tile_height}mm, ' + 
+            f'width={tile_width}mm, ' +
+            'keepaspectratio=FALSE'
+            ),
+        arguments=image_path)
+    
+    add_horizontal_crop_marks(doc, x_pos, y_pos, tile_width, tile_height)
+    add_vertical_crop_marks(doc, x_pos, y_pos, tile_width, tile_height)
+    with doc.create(MiniPage(width=NoEscape(f"{tile_width}mm"))) as minipage:
+        if tile_width > tile_height:
+            minipage.append(
+                Command('rotatebox',
+                        arguments='90',
+                        extra_arguments=image_command)
+            )
+        else:
+            minipage.append(image_command)
+
+def calculate_cut_lines_and_tiles(
+        tile_columns: int,
+        tile_rows: int,
+        total_board_width: float = 832, # in mm
+        total_board_height: float = 589, # in mm
         outer_margin: float = 5, # in mm
         inner_margin: float = 1 # in mm
-    ):
-    # Open the image
-    img = Image.open(image_path)
-    
-    # Calculate effective width and height
-    effective_width = total_width - 2 * outer_margin
-    effective_height = total_height - 2 * outer_margin
-    
+    ) -> tuple[list[tuple[float, float, float, float]], list[float], list[float]]:
     # Calculate tile widths and heights
-    tile_widths = [total_width / n - 2*inner_margin] * n
-    tile_heights = [total_height / m - 2*inner_margin] * m
-    # Correct outer cell sizes to account for outer margin
+    tile_widths = [total_board_width / tile_columns - 2 * inner_margin] * tile_columns
+    tile_heights = [total_board_height / tile_rows - 2 * inner_margin] * tile_rows
+
     tile_widths[0] -= outer_margin - inner_margin
     tile_widths[-1] -= outer_margin - inner_margin
     tile_heights[0] -= outer_margin - inner_margin
     tile_heights[-1] -= outer_margin - inner_margin
 
+    tiles = [] # list of bboxes as (left, bottom, width, height)
+    vertical_cut_lines = []
+    horizontal_cut_lines = []
+    
+    for i in range(tile_columns):
+        for j in range(tile_rows):
+            x = outer_margin + sum(tile_widths[:i]) + 2 * i * inner_margin
+            y = outer_margin + sum(tile_heights[:j]) + 2 * j * inner_margin
+            tile = (x, y, tile_widths[i], tile_heights[j])
+            tiles.append(tile)
+    
+    for i in range(1, tile_columns):
+        cut_x = outer_margin + sum(tile_widths[:i]) + (2 * i - 1) * inner_margin
+        vertical_cut_lines.append(cut_x)
+    
+    for j in range(1, tile_rows):
+        cut_y = outer_margin + sum(tile_heights[:j]) + (2 * j - 1) * inner_margin
+        horizontal_cut_lines.append(cut_y)
+    
+    return tiles, horizontal_cut_lines, vertical_cut_lines
+
+def plot_tiles_and_cut_lines(
+        image_path: str,
+        tile_bboxes: list[tuple[float, float, float, float]],
+        horizontal_cut_lines: list[float],
+        vertical_cut_lines: list[float],
+        total_board_width: float,
+        total_board_height: float,
+    ):
+    """
+    Given an image and the bounding boxes of tiles and cut lines plot all on a board of the given site to visualize tiles and cut lines
+
+    Args:
+        image_path (str): 
+        tile_bboxes (list[tuple[float, float, float, float]]): 
+        horizontal_cut_lines (list[float]): 
+        vertical_cut_lines (list[float]): 
+        total_board_width (float): 
+        total_board_height (float): 
+    """
+    # Open the image
+    img = Image.open(image_path)
+    
     # Create figure and axes
     fig, ax = plt.subplots(1)
     
-    # Draw the total size rectangle
-    total_rect = patches.Rectangle((0, 0), total_width, total_height, linewidth=1, edgecolor='b', facecolor='none')
+    # Draw the total board size rectangle
+    total_rect = patches.Rectangle((0, 0), total_board_width, total_board_height, linewidth=1, edgecolor='b', facecolor='none')
     ax.add_patch(total_rect)
     
     # Plot rectangles for tiles
-    for i in range(n):
-        for j in range(m):
-            x = outer_margin + sum(tile_widths[:i]) + 2*i * inner_margin
-            y = outer_margin + sum(tile_heights[:j]) + 2*j * inner_margin
-            rect = patches.Rectangle((x, y), tile_widths[i], tile_heights[j], linewidth=1, edgecolor='r', facecolor='none')
-            ax.add_patch(rect)
+    min_x, min_y, max_x, max_y = float("inf"), float("inf"), float("-inf"), float("-inf")
+    for tile in tile_bboxes:
+        x, y, w, h = tile
+        if x < min_x:
+            min_x = x
+        if y < min_y:
+            min_y = y
+        if x + w > max_x:
+            max_x = x + w
+        if y + h > max_y:
+            max_y = y + h
+        patch = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(patch)
     
-    # Plot the image centered within the effective area
-    img_x = outer_margin
-    img_y = outer_margin
-    img_extent = [img_x, img_x + effective_width, img_y + effective_height, img_y]
+    # Get image extent as min and max in rectangles
+    img_extent = [min_x, max_x, min_y, max_y] # (left, right, bottom, top)
     ax.imshow(img, extent=img_extent)
     
     # Draw cut lines for inner margins
-    for i in range(1, n): # vertical cut lines
-        cut_x = outer_margin + sum(tile_widths[:i]) + (2*i-1) * inner_margin
-        print(f"vert cut {i} at {cut_x}")
+    for cut_x in vertical_cut_lines:  # vertical cut lines
         ax.axvline(x=cut_x, color='g', linestyle='--')
     
-    for j in range(1, m): # horizontal cut lines
-        cut_y = outer_margin + sum(tile_heights[:j]) + (2*j-1) * inner_margin
-        print(f"horz cut {j} at {cut_y}")
+    for cut_y in horizontal_cut_lines:  # horizontal cut lines
         ax.axhline(y=cut_y, color='g', linestyle='--')
     
     # Set limits and aspect ratio
-    ax.set_xlim(0, total_width)
-    ax.set_ylim(total_height, 0)
+    ax.set_xlim(-2, total_board_width+2)
+    ax.set_ylim(-2, total_board_height+2)
     ax.set_aspect('equal')
     
     plt.show()
 
+def export_sub_images(
+        image_path: str,
+        output_folder: str,
+        tiles,
+        total_board_width,
+        total_board_height,
+        output_prefix='cut') -> list[str]:
+    img = Image.open(image_path)
+    img_width, img_height = img.size
+    tile_columns = len(set([x for x, *_ in tiles]))
+    tile_rows = len(set([y for _, y, *_ in tiles]))
+    # convert mm to pixels
+    scale_x = img_width / total_board_width
+    scale_y = img_height / total_board_height
+    
+    image_paths = []
+    for idx, tile in enumerate(tiles):
+        x, y, w, h = tile
+        left = int(x * scale_x)
+        upper = int(y * scale_y)
+        right = int((x + w) * scale_x)
+        lower = int((y + h) * scale_y)
+        
+        cropped_img = img.crop((left, upper, right, lower))
+        filename = f"{output_prefix}_{idx%tile_columns+1}_{idx//tile_rows+1}.png"
+        filepath = os.path.join(output_folder, filename)
+        image_paths.append(filepath)
+        cropped_img.save(filepath)
+        print(f"Saved sub-image {filename} to {output_folder}")
+
+    return image_paths
+
+def generate_board_latex(
+        image_paths: list[str],
+        tile_bboxes: list[tuple[float, float, float, float]],
+        target_filepath: str,
+        border: tuple[float, float] = (10, 10),
+    ):
+    if border[0] != border[1]:
+        raise NotImplementedError("Different borders for left and top are not supported yet.")
+
+    geometry_options = {
+        "margin": f"{border[1]}mm"
+    }
+    doc = Document(documentclass='article', document_options='a4paper', geometry_options=geometry_options)
+    doc.packages.append(Package('graphicx'))
+    doc.packages.append(Package('tikz'))
+    doc.append(NoEscape(r'\pagestyle{empty}'))
+
+    x_pos = border[0]
+    y_pos = border[1]
+    
+    for tile, image_path in zip(tile_bboxes, image_paths):
+        image_path: str = image_path.replace("\\", "/")
+        add_fullpage_image(
+            doc=doc,
+            x_pos=x_pos,
+            y_pos=y_pos,
+            image_path=image_path,
+            tile_width=tile[2],
+            tile_height=tile[3]
+        )
+        doc.append(NoEscape(r'\newpage'))
+    # Output as .tex file
+    target_filepath_stripped = target_filepath.strip(".tex")
+    doc.generate_tex(target_filepath_stripped)
+    print(f"Generated LaTeX file at {target_filepath}")
 
 if __name__ == "__main__":
     # Hide the main Tkinter window
     root = Tk()
     root.withdraw()
 
-    # Open a file picker dialog to choose the image file
-    image_path = filedialog.askopenfilename(
-        title='Select an image file',
-        filetypes=[('Image files', '*.png;*.jpg;*.jpeg;*.bmp')]
-    )
+    image_path = None
+    image_path = "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/board_samples/06_board_preview.png"
+    image_paths = None
+    image_paths = [
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_1_1.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_1_2.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_1_3.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_2_1.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_2_2.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_2_3.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_3_1.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_3_2.png",
+        "C:/Users/basti/Documents/programming/python/Beleriand_TTR/projects/MiddleEarth_TTR/printing/board_06/Middle_Earth_3_3.png"
+    ]
 
-    # # Open a folder picker dialog to choose the output directory
-    # output_folder = filedialog.askdirectory(title='Select an output folder')
+    if not image_path:
+        # Open a file picker dialog to choose the image file
+        image_path = filedialog.askopenfilename(
+            title='Select an image file',
+            filetypes=[('Image files', '*.png;*.jpg;*.jpeg;*.bmp')]
+        )
+
+    if not image_paths:
+        # # Open a folder picker dialog to choose the output directory
+        output_folder = filedialog.askdirectory(title='Select an output folder')
+    else:
+        output_folder = os.path.dirname(image_paths[0])
 
     # split_image_into_4_parts(image_path, output_folder)
+    tile_columns = 3
+    tile_rows = 3
+    total_board_width = 832 # in mm
+    total_board_height = 589 # in mm
+    outer_margin = 2 # in mm
+    inner_margin = 1 # in mm
     
-    split_image_nxm(
-        image_path,
-        3, 3,
-        total_width = 832, # in mm
-        total_height = 589, # in mm
-        outer_margin = 2, # in mm
-        inner_margin = 1 # in mm
+    tile_bboxes, horz_cuts, vert_cuts = calculate_cut_lines_and_tiles(
+        tile_columns = tile_columns,
+        tile_rows = tile_rows,
+        total_board_width = total_board_width, # in mm
+        total_board_height = total_board_height, # in mm
+        outer_margin = outer_margin, # in mm
+        inner_margin = inner_margin # in mm
+    )
+    # plot_tiles_and_cut_lines(
+    #     image_path,
+    #     tile_bboxes,
+    #     horz_cuts,
+    #     vert_cuts,
+    #     total_board_width = total_board_width,
+    #     total_board_height = total_board_height,
+    # )
+    if not image_paths:
+        image_paths = export_sub_images(
+            image_path,
+            output_folder,
+            tile_bboxes,
+            total_board_width,
+            total_board_height,
+            output_prefix='Middle_Earth'
+        )
+    generate_board_latex(
+        image_paths=image_paths,
+        tile_bboxes=tile_bboxes,
+        target_filepath=f"{output_folder}/Middle_Earth.tex",
+        border=(10, 10),
     )
